d46865284e7943ff4025dcc3ae3eb201f1da7b3f
ryansmane <ryanzimmer8@gmail.com>
2020-05-11 16:57:26 -0400

    Updates

---
+++ precache-manifest.19be9c1519d1ae549f832fd8d54ab5fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e43fb091a7a6c062826910b5d679990",
+    "url": "/war_client/index.html"
+  },
+  {
+    "revision": "a88537fd2ae834e2032f",
+    "url": "/war_client/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dbb74e0e4d4bb148d467",
+    "url": "/war_client/static/css/main.877c2b02.chunk.css"
+  },
+  {
+    "revision": "a88537fd2ae834e2032f",
+    "url": "/war_client/static/js/2.b5bd2d3e.chunk.js"
+  },
+  {
+    "revision": "fa7ee0158af47db62330833907bf1bdc",
+    "url": "/war_client/static/js/2.b5bd2d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb74e0e4d4bb148d467",
+    "url": "/war_client/static/js/main.fe33ee3f.chunk.js"
+  },
+  {
+    "revision": "2343d9f4e41e5dfc64ef",
+    "url": "/war_client/static/js/runtime-main.524d7ee6.js"
+  }
+]);
