b04c9ec91920fb14de5e44c910136d75dff133e1
ryansmane <ryanzimmer8@gmail.com>
2020-05-12 09:13:49 -0400

    pathing fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div className='header-box'>
-            <img className='logo' src='/images/logo.png' alt='a cute shark holding cards'></img>
+            <img className='logo' src='./images/logo.png' alt='a cute shark holding cards'></img>
             <div className='title-box'>
             <h1>Shark Haven</h1>
             </div>
