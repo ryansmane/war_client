d0ee7a50161379f45228ece0bc436e8a7a634521
ryansmane <ryanzimmer8@gmail.com>
2020-05-12 09:28:13 -0400

    pathing fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-   <Router basename='/war_client'>
+   <Router basename='/war-client'>
       <App />
    </Router>,
    document.getElementById('root')
